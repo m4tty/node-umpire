3c0f3ec235b16b4224cc2e955fbea4e95756dd76
Matt Self <self.matt@gmail.com>
2013-04-22 21:24:01 -0600

    fixes #3. supported multiple target returned from graphite raw

--- test/numpire.test.js
+++ test/numpire.test.js
@@ -23,6 +23,13 @@ describe('metric processing', function() {
 			});
 		});
 		it('should map graphite format multi-dimensional array to flat array', function(done) {
+			var graphiteData = "[{\"target\":\"stats.timers.test.test-rest.messages.saved.mean_90\",\"datapoints\":[[2.7291666666666665,1366221770],[2.8133333333333335,1366221780],[2.811111111111111,1366221790],[2.8421052631578947,1366221800],[2.727272727272727,1366221810],[null,1366221820]]},{\"target\":\"stats.timers.test.test-rest.messages.saved.mean_90\",\"datapoints\":[[2.7291666666666665,1366221770],[2.8133333333333335,1366221780],[2.811111111111111,1366221790],[2.8421052631578947,1366221800],[2.727272727272727,1366221810],[null,1366221820]]}]";
+			processing.mapGraphiteData(graphiteData, function(err, data) {
+				assert.deepEqual(data, [ 2.7291666666666665, 2.8133333333333335, 2.811111111111111, 2.8421052631578947, 2.727272727272727, null ,2.7291666666666665, 2.8133333333333335, 2.811111111111111, 2.8421052631578947, 2.727272727272727, null]);
+				done();
+			});
+		});
+		it('should map graphite format multi-dimensional array to flat array', function(done) {
 			var graphiteData = "[{\"target\":\"stats.timers.test.test-rest.messages.saved.mean_90\",\"datapoints\":[[2.7291666666666665,1366221770],[2.8133333333333335,1366221780],[2.811111111111111,1366221790],[2.8421052631578947,1366221800],[2.727272727272727,1366221810],[null,1366221820]]}]";
 			var isOk = null;
 			isOk = processing.checkMetricsArrayAgainstParams([ 2.7291666666666665, 2.8133333333333335, 2.811111111111111, 2.8421052631578947, 2.727272727272727, null],0,3);
